8e7219f2d8daacddd44c4d2d8f706b0f4e1fea3a
DamianTodorowski <115210660+DamianTodorowski@users.noreply.github.com>
2023-04-21 12:23:41 +0200

    Update Kolos przyklad.py

--- Kolos przyklad.py
+++ Kolos przyklad.py
@@ -169,3 +169,30 @@ print(result)
 # 32
 
 
+#szyfrowanie 
+def encrypt(string, encryptKey):
+    resultString = ''
+    for x in string:
+        if x in encryptKey:
+            resultString += encryptKey[x]
+        else:
+            resultString += x
+    return resultString
+
+def decrypt(string,encryptKey):
+    resultString = ''
+    for x in string:
+        if x in encryptKey.values():
+            for key,value in encryptKey.items():
+                if value==x:
+                    resultString += key
+        else:
+            resultString += x
+    return resultString
+
+
+strung = "informatyka"
+klucz = {'a':'y', 'e':'i', 'i':'o' , 'o':'a' , 'y':'e'}
+strung2 = "onfarmyteky"
+print(encrypt(strung,klucz))
+print(decrypt(strung2,klucz))
